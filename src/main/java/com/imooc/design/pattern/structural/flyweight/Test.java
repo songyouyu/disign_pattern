7c5665f6baf0d1bef8326004a13acd1c2ff14f56
songyouyu <songyy950208@163.com>
2018-12-13 10:37:46 +0800

    享元模式

--- src/main/java/com/imooc/design/pattern/structural/flyweight/Test.java
+++ src/main/java/com/imooc/design/pattern/structural/flyweight/Test.java
@@ -7,7 +7,7 @@ package com.imooc.design.pattern.structural.flyweight;
  */
 public class Test {
 
-    private static final String departments[] = {"RD","QA","PM","BD"};
+    private static final String[] departments = {"RD","QA","PM","BD"};
 
     public static void main(String[] args) {
         for(int i = 0; i < 10; i++){
